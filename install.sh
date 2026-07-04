d4cfd72f710cabd1e66e7e76b3413f26b938dda0
Aukan <aukan@operator.com>
2026-07-04 15:33:02 -0400

    Update

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ qogir-cursor-theme tokyonight-gtk-theme
 log "Instalando herramientas BlackArch"
 
 sudo pacman -S --noconfirm --needed \
-impacket radare2 arp-scan ncat nmap ffuf hashcat wpscan responder \
+impacket radare2 arp-scan ncat nmap testssl.sh ffuf hashcat wpscan responder \
 macchanger netexec whatweb nuclei dnsx subfinder chaos-client uncover \
 naabu httpx metasploit evil-winrm caido seclists powersploit
 
